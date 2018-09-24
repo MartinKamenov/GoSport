f0107d7e15dbb8464b65242bfeead34331235fa2
Martin Kamenov <martinkamenov10@gmail.com>
2018-09-24 13:43:17 +0300

    Added image view for the found locations

--- GoSportBG/app/src/main/java/com/kamenov/martin/gosportbg/custom_locations/CustomLocationsFragment.java
+++ GoSportBG/app/src/main/java/com/kamenov/martin/gosportbg/custom_locations/CustomLocationsFragment.java
@@ -1,26 +1,28 @@
 package com.kamenov.martin.gosportbg.custom_locations;
 
-
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.kamenov.martin.gosportbg.R;
 import com.kamenov.martin.gosportbg.base.contracts.BaseContracts;
+import com.kamenov.martin.gosportbg.constants.Constants;
+import com.kamenov.martin.gosportbg.internet.DownloadImageTask;
 import com.kamenov.martin.gosportbg.models.CustomLocation;
+import com.kamenov.martin.gosportbg.models.optimizators.ImageCachingService;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class CustomLocationsFragment extends Fragment implements CustomLocationsContracts.ICustomLocationsView {
     private CustomLocationsContracts.ICustomLocationsPresenter mPresenter;
     private View root;
     private LinearLayout mLocationsContainer;
+    private ImageCachingService imageCachingService;
 
     public CustomLocationsFragment() {
         // Required empty public constructor
@@ -32,6 +34,7 @@ public class CustomLocationsFragment extends Fragment implements CustomLocations
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         root = inflater.inflate(R.layout.fragment_custom_locations, container, false);
+        imageCachingService = ImageCachingService.getInstance();
         mLocationsContainer = root.findViewById(R.id.locations_container);
         return root;
     }
@@ -49,9 +52,21 @@ public class CustomLocationsFragment extends Fragment implements CustomLocations
                 hideProgressBar();
                 for(int i = 0; i < customLocations.length; i++) {
                     CustomLocation location = customLocations[i];
-                    TextView textView = new TextView(getActivity());
-                    textView.setText(location.name);
-                    mLocationsContainer.addView(textView);
+                    View img;
+                    if(location.pictureUrl != null && !imageCachingService.hasBitmap(location.pictureUrl)) {
+                        img = new ProgressBar(getActivity());
+                        new DownloadImageTask((ProgressBar)img, getActivity())
+                                .execute(location.pictureUrl);
+                    } else if(location.pictureUrl == null) {
+                        img = new ProgressBar(getActivity());
+                        new DownloadImageTask((ProgressBar)img, getActivity())
+                                .execute(Constants.DOMAIN + "/static/images/locations/default.jpg");
+                    } else {
+                        img = new CircleImageView(getActivity());
+                        ((CircleImageView)img).setImageBitmap(imageCachingService.getBitmap(location.pictureUrl));
+                    }
+
+                    // mLocationsContainer.addView(img);
                 }
             }
         });
@@ -75,5 +90,6 @@ public class CustomLocationsFragment extends Fragment implements CustomLocations
     @Override
     public void hideProgressBar() {
         root.findViewById(R.id.locations_progressbar).setVisibility(View.GONE);
+        mLocationsContainer.setVisibility(View.VISIBLE);
     }
 }
